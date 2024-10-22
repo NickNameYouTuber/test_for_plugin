6b0cc96d8accccc1264217f762e27280e440ec29
NickNameYouTuber <>
2024-10-22 17:42:21 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-            System.out.printf("ааав!");
+            System.out.printf("аааввы!");
         }
 
         if (1 == 1){
