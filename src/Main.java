8366908be3c202a36672205b504b6d83fa7639a1
NickNameYouTuber <>
2024-10-23 02:01:44 +0300

    Написать Hello world_1
    Updated files:
    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hi Negr!");
-        System.out.printf("AHAHHAHA!");
-        System.out.printf("AHAHHAHA!");
+        System.out.printf("в!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
