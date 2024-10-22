30a3079c0c712de9202ed957267d96198c01207d
NickNameYouTuber <>
2024-10-23 02:01:04 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hi Negr!");
         System.out.printf("AHAHHAHA!");
+        System.out.printf("AHAHHAHA!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
