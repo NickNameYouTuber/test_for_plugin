8a9408cfc5e9f1fbe53427c0e09249203ce6e9da
NickNameYouTuber <>
2024-10-25 01:41:38 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("в!");
         System.out.printf("в!");
+        System.out.printf("в!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -21,8 +22,6 @@ public class Main {
             System.out.println(sub(1, 3));
         }
 
-        System.out.printf("END!");
-        System.out.printf("END!");
         System.out.printf("END!");
         System.out.printf("END!");
     }
