1af6635585254670226ec282f73b34c8c8e99c24
NickNameYouTuber <>
2024-10-24 17:28:10 +0300

    Написать Hello world_1

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,10 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("в!");
-        System.out.println("A");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
             System.out.printf("XZ!");
-            System.out.printf("в!");
-            System.out.println("A");
         }
 
         if (1 == 1){
