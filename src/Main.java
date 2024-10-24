4b79064c21bc1cc996ef45aba4c83ceba112fcec
NickNameYouTuber <>
2024-10-24 18:02:37 +0300

    Написать Hello world_2

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,11 @@ public class Main {
         System.out.printf("END!");
         System.out.printf("END!");
         System.out.printf("END!");
+        System.out.printf("END!");
+        System.out.printf("END!");
+        System.out.printf("END!");
+        System.out.printf("END!");
+        System.out.printf("END!");
     }
 
     public static float sum(int a, int b){
